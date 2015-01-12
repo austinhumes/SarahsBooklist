cc6d7773d6f6a13da0a3dec8054cc71e3ca5b389
Austin Humes <austin@rakenapp.com>
2015-01-12 12:44:56 -0800

    Pulling book list from api

--- app/src/main/java/com/austinhumes/sarahsbooklist/MainActivity.java
+++ app/src/main/java/com/austinhumes/sarahsbooklist/MainActivity.java
@@ -1,9 +1,20 @@
 package com.austinhumes.sarahsbooklist;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +23,37 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Pull list from api
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://austinhumes.com/sarah/get";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+            new Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject jsonObject) {
+                    Log.d("blah", "Response is: " + jsonObject);
+                }
+            },
+            new ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
+
+    }
+    /*
+    StringBuilder url = new StringBuilder("http://austinhumes.com/sarah/get");
+    JSONRequest editTask = new JSONRequest(Request.Method.GET,
+            url.toString(), null, successListener(), errorListener());
+
+    DataService.getInstance().addToRequestQueue(editTask);
+    */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +76,13 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*protected Listener<JSONObject> successListener() {
+        return new Response.Listener<JSONObject>() {
+
+            public void onResponse(JSONObject jsonObject) {
+
+            }
+        };
+    }*/
 }
